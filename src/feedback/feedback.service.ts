37d7ce0e2a2be6a6e425a90791d9b169d338aa63
unknown <ktk8916@naver.com>
2022-10-13 16:37:01 +0900

    FEAT : Add create feedback service for memory db

--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CreateFeedbackDto } from './dto/createFeedback.dto';
+import { Feedback } from './entity/feedback.entity';
 
 @Injectable()
-export class FeedbackService {}
+export class FeedbackService {
+    private sequence = 0;
+    private feedbacks:Feedback[] = [];
+
+    createFeedback(createFeedbackDto:CreateFeedbackDto){
+        this.sequence++;
+        const feedCreateTime = '아무튼시간임';
+        const {feedContent} = createFeedbackDto;
+
+        const feedback:Feedback = {
+            feedbackId: this.sequence,
+            feedContent,
+            feedCreateTime
+        }
+
+        this.feedbacks.push(feedback);
+    }
+
+}
