f323c99f55944acd85dfcae1de32e2f69f45fb41
unknown <ktk8916@naver.com>
2022-10-14 14:57:43 +0900

    FEAT : Add recruitment entity

---
+++ src/recruitment/entity/recruitment.entity.ts
@@ -0,0 +1,14 @@
+export class Recruitment{
+    recruitmentId:number;
+    recruitmentCategory:string;
+    recruitmentTitle:string;
+    recruitmentContent:string;
+    openTalkLink:string;
+    recruitmentCreateTime:string;
+    recruitmentViews:number;
+    recruitmentIsEnded:boolean;
+    recruitmentIsDeleted:boolean;
+    recruitmentWriterName:string;
+    recruitmentWriterPassword:string;
+    recruitmentWriterIp:string;
+} 
