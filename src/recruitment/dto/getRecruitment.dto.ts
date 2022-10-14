919175831051efc100b075a5a876b968ee53a878
unknown <ktk8916@naver.com>
2022-10-14 15:52:05 +0900

    FEAT : Add get recruitment dto

---
+++ src/recruitment/dto/getRecruitment.dto.ts
@@ -0,0 +1,13 @@
+export class getRecruitmentDto{
+    recruitmentId:number;
+    recruitmentCategory:string;
+    recruitmentTitle:string;
+    recruitmentContent:string;
+    openTalkLink:string;
+    recruitmentCreateTime:string;
+    recruitmentViews:number;
+    recruitmentIsEnded:boolean;
+    recruitmentWriterName:string;
+    recruitmentWriterPassword:string;
+    recruitmentWriterIp:string;
+} 
