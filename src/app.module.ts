cd354a8924e72aefe3d10f74859c04652929c9f2
ktk8916 <ktk8916@naver.com>
2022-10-15 23:15:59 +0900

    FEAT : Import typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeedbackModule } from './feedback/feedback.module';
 import { RecruitmentModule } from './recruitment/recruitment.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [FeedbackModule, RecruitmentModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    FeedbackModule,
+    RecruitmentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
