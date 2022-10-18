3e3125720032238ddeefd35eaaa0010283058fe7
ktk8916 <ktk8916@naver.com>
2022-10-18 16:21:01 +0900

    FEAT : DB 연결 설정 파일 형식 추가

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,8 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Feedback } from "src/feedback/entity/feedback.entity";
+import { Recruitment } from "src/recruitment/entity/recruitment.entity";
+
+
+export const typeORMConfig : TypeOrmModuleOptions = {
+    
+}
